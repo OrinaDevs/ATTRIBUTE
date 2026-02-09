8c53f808dc44978dbb11404e8637a29d20fb0620
Orinah <orinaallan38@gmail.com>
2026-02-09 10:19:32 +0300

    Added Staff OTP Model

--- attribute_web/accounts/models.py
+++ attribute_web/accounts/models.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import random
 
 # Create your models here.
 class CustomUser(AbstractUser):
@@ -16,3 +18,13 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+#Staff OTP Model
+class StaffOTP(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    code = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_used = models.BooleanField(default=False)
+
+    def generate_code(self):
+        self.code = str(random.randind(100000, 999999))
